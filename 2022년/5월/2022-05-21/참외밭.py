12c2e1ea6e16c7c5a185661e3929836eb8b77cd3
hanjiho <hanjiho97@gist.ac.kr>
2022-05-22 23:36:57 +0900

    day2

--- 2022년/5월/2022-05-21/참외밭.py
+++ 2022년/5월/2022-05-21/참외밭.py
@@ -1,7 +1,26 @@
+from collections import deque
 K=int(input())
 
-rec=[]
+rec=deque()
+count=[0,0,0,0,0]
 for _ in range(6):
     d,m=map(int, input().split())
+    count[d]+=1
     rec.append((d,m))
-print(rec)
+    
+if count[1]==1:
+    if count[3]==1:
+        while rec[0][0]!=3:
+            rec.rotate()
+    else:
+        while rec[0][0]!=1:
+            rec.rotate()
+else:
+    if count[4]==1:
+        while rec[0][0]!=4:
+            rec.rotate()
+    else:
+        while rec[0][0]!=2:
+            rec.rotate()
+
+print((rec[0][1]*rec[1][1]-rec[3][1]*rec[4][1])*K)
